f2821b390fe97829133e15d8489256152b98a349
Jake Walker <jakewalk93@hotmail.com>
2021-12-08 20:23:40 +0000

    Built backgroundButton component that changes background on click

---
+++ pages/backgroundButton.tsx
@@ -0,0 +1,31 @@
+import styles from '../styles/backgroundButton.module.css'
+import Draggable from 'react-draggable' // Thank you. Grateful.
+import React, { useState } from "react";
+
+const BackgroundButton = () => {
+
+    const [color, setColor] = useState('white');
+
+    function change(event) {
+
+        if (event.cancelable) {
+            event.preventDefault();
+        }
+
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        setColor(color)
+    }
+
+    return <div className={styles.background} style={{ background: color }}>
+        <Draggable>
+            <button className={styles.button} onClick={change}>Change Colour</button>
+        </Draggable>
+    </div >
+}
+
+export default BackgroundButton
