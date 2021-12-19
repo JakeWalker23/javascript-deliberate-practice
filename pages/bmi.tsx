e20e3cfd1cb9943e9b63b0eca10e1cfac2ce9d0b
Jake Walker <jakewalk93@hotmail.com>
2021-12-19 22:02:15 +0000

    Added BMI calculator page component with functionality but no styling.

--- pages/bmi.tsx
+++ pages/bmi.tsx
@@ -1,5 +1,70 @@
+import React, { useState } from "react";
+import styles from '../styles/bmi.module.css'
+
 const BMI = () => {
-    return (<h1>Hello</h1>)
+
+    const [data, setData] = useState({
+        weight: "",
+        height: ""
+    });
+
+    const [bmi, setBMI] = useState(0)
+
+    function handle(e) {
+        const newData = { ...data };
+        newData[e.target.id] = e.target.value;
+        setData(newData);
+    }
+
+    function BMI(height, weight) {
+        return (weight / (height * height)) * 703
+    }
+
+    function calculateInches(feet) {
+        return feet * 12
+    }
+
+    function calculateBMI() {
+        const height = calculateInches(data.height)
+
+        setBMI(BMI(height, data.weight))
+    }
+
+    return (
+        <div>
+            <h4>BMI Calculator</h4>
+
+            <label>
+                Please Enter your weight
+                <input placeholder="lbs" onChange={(e) => handle(e)} id="weight" required
+                ></input>
+            </label>
+
+            <label>
+                Please Enter your height
+                <input placeholder="feet" onChange={(e) => handle(e)} id="height" required></input>
+            </label>
+
+            <button onClick={calculateBMI}>Calculate</button>
+
+            <div>
+                {bmi}
+            </div>
+        </div >
+    )
 }
 
 export default BMI
+
+/*
+
+TODO
+
+Use a GUI interface with sliders for height and weight.
+
+Update the user interface on the fly. Use colors as well
+as text to indicate health.
+
+
+
+*/
